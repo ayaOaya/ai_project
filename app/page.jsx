d87ac262b7c883d689d1e4300aaae81fb8c71479
ayaOaya <alyaauaacn@gmail.com>
2023-05-18 23:22:06 +0200

    add google clientID and clientSEcret

--- app/page.jsx
+++ app/page.jsx
@@ -1,8 +1,21 @@
+import Feed from "@components/Feed"
 
 const Home = () => {
   return (
-    <div>Home</div>
-  )
+    <section className="w-full flex-center flex-col">
+      <h1 className="head_text text-center">
+        Discover & Share
+        <br  className="max-md:hidden"/>
+        <span className="orange_gradient">
+          AI-Powered Prompts
+          </span>
+        </h1>
+        <p className="desc text-center">
+          Lorem ipsum amet consectetur, adipisicing elit. Fuga possimus laboriosam, a nulla odio quidem est ea cupiditate nemo sapiente dolorum consequatur, pariatur odit assumenda minima officia dicta. Laudantium,.
+        </p>
+        <Feed />
+    </section>
+  ) 
 }
 
 export default Home
